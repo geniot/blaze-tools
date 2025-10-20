96d24391cd0f7781665df5f7bcfa28c214c83d8b
Vitali Sazanovich <vitali.sazanovich@koop.overheid.nl>
2025-10-21 00:36:01 +0200

    Make reset generic

--- src/configuration.go
+++ src/configuration.go
@@ -6,10 +6,9 @@ import (
 )
 
 type Config struct {
-	ServerHost  string `default:"localhost" validate:"required" envvar:"SERVER_HOST"`
-	ServerPort  int    `default:"8444" validate:"required" envvar:"SERVER_PORT"`
-	DatabaseUrl string `default:"postgres://blaze_test:blaze_test@localhost:9876/blaze_test" validate:"required" envvar:"DATABASE_URL"`
-	ResetToken  string `default:"sometoken" validate:"required" envvar:"RESET_TOKEN"`
+	ServerHost string `default:"localhost" validate:"required" envvar:"SERVER_HOST"`
+	ServerPort int    `default:"8444" validate:"required" envvar:"SERVER_PORT"`
+	ResetToken string `default:"sometoken" validate:"required" envvar:"RESET_TOKEN"`
 }
 
 func newConfig() *Config {
--- src/utils.go
+++ src/utils.go
@@ -3,7 +3,15 @@ package main
 import (
 	"bytes"
 	"embed"
+	"encoding/csv"
+	"fmt"
 	"hash/fnv"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 func GetFileBytes(fileName string, fs *embed.FS) []byte {
@@ -43,4 +51,70 @@ func If[T any](cond bool, vTrue, vFalse T) T {
 		return vTrue
 	}
 	return vFalse
+}
+
+// Generates a password hash for the 'admin' password.
+// This is the default password for a new installation. See migrations/000002_create_user_admin.up.sql
+func genAdminCredentials() {
+	var (
+		password = "admin"
+	)
+	println(password)
+	passwordHashBbs, _ := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	println(string(passwordHashBbs))
+	if bcrypt.CompareHashAndPassword(passwordHashBbs, []byte(password)) != nil {
+		println("Password hash does not match")
+	}
+}
+
+// Generates 000005_insert_content_categories.up.sql with inserts for content_category.
+func genCategories() {
+	var (
+		tableName      = " content_category "
+		columns        = "(id,parent_id,name,tier_1,tier_2,tier_3,tier_4)"
+		values         = "(%s,%s,%s,%s,%s,%s,%s)"
+		insertTemplate = "insert into" + tableName + columns + " values " + values + ";"
+		buffer         bytes.Buffer
+	)
+
+	inReader := csv.NewReader(bytes.NewReader(GetFileBytes("res/Content Taxonomy 3.1.tsv", &resources)))
+	inReader.Comma = '\t'
+	records, err := inReader.ReadAll()
+	if err != nil {
+		println(err.Error())
+	}
+	records = records[2:] // reslice to omit header
+	for _, record := range records {
+		if !IsDigitsOnly(record[0]) {
+			record[0] = strconv.Itoa(Hash(record[0]))
+		}
+		if !IsDigitsOnly(record[1]) {
+			record[1] = strconv.Itoa(Hash(record[1]))
+		}
+	}
+	sort.Slice(records, func(i, j int) bool {
+		if records[i][1] == "" {
+			return true
+		}
+		if records[j][1] == "" {
+			return false
+		}
+		return First(strconv.Atoi(records[i][0])) < First(strconv.Atoi(records[j][0]))
+	})
+	for _, record := range records {
+		buffer.WriteString(fmt.Sprintf(insertTemplate,
+			If(record[0] == "", "null", record[0]),
+			If(record[1] == "", "null", record[1]),
+			If(record[2] == "", "null", "'"+strings.ReplaceAll(record[2], "'", "''")+"'"),
+			If(record[3] == "", "null", "'"+strings.ReplaceAll(record[3], "'", "''")+"'"),
+			If(record[4] == "", "null", "'"+strings.ReplaceAll(record[4], "'", "''")+"'"),
+			If(record[5] == "", "null", "'"+strings.ReplaceAll(record[5], "'", "''")+"'"),
+			If(record[6] == "", "null", "'"+strings.ReplaceAll(record[6], "'", "''")+"'"),
+		))
+		buffer.WriteString("\n")
+	}
+	err = os.WriteFile("000005_insert_content_categories.up.sql", buffer.Bytes(), 0644)
+	if err != nil {
+		println(err.Error())
+	}
 }
